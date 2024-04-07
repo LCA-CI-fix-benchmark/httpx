d25975e58491600d7b26c7c56878a56e5634ecc8
jovyan <jovyan@kovrigin-a100-80gb-0>
2024-04-07 21:08:19 +0000

    test_lca_code_editing__code-editing-metrics-x6c85ba4e9303a878__id_105

--- httpx/_client.py
+++ httpx/_client.py
@@ -168,6 +168,10 @@ class BaseClient:
         timeout: TimeoutTypes = DEFAULT_TIMEOUT_CONFIG,
         follow_redirects: bool = False,
         max_redirects: int = DEFAULT_MAX_REDIRECTS,
+    def __getitem__(self, name: str) -> str:
+        value = self.get(name)
+        if value is None:
+            raise KeyError(name)
         event_hooks: typing.Optional[
             typing.Mapping[str, typing.List[EventHook]]
         ] = None,
