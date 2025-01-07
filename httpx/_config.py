1a109d4e9d12f78259dc8660d9f30252b5c1a47b
Alexander Kovrigin <alexander.kovrigin@jetbrains.com>
2025-01-07 18:41:34 +0100

    waleko__diff-9d9b3__id_106

--- httpx/_config.py
+++ httpx/_config.py
@@ -1,15 +1,14 @@
+import certifi
 import logging
 import os
 import ssl
 import typing
 from pathlib import Path
 
-import certifi
-
 from ._compat import set_minimum_tls_version_1_2
 from ._models import Headers
-from ._types import CertTypes, HeaderTypes, TimeoutTypes, URLTypes, VerifyTypes
 from ._urls import URL
+from ._types import CertTypes, HeaderTypes, TimeoutTypes, URLTypes, VerifyTypes
 from ._utils import get_ca_bundle_from_env
 
 
