773f3ea029656b270fc1184af8fb3ef1d6241ca0
ubuntu <ubuntu@ip-10-0-1-17>
2025-01-10 15:40:25 +0000

    waleko__gemini-pro-1.5_8afe__id_106

--- httpx/_config.py
+++ httpx/_config.py
@@ -3,9 +3,6 @@ import os
 import ssl
 import typing
 from pathlib import Path
-
-import certifi
-
 from ._compat import set_minimum_tls_version_1_2
 from ._models import Headers
 from ._types import CertTypes, HeaderTypes, TimeoutTypes, URLTypes, VerifyTypes
@@ -13,6 +10,7 @@ from ._urls import URL
 from ._utils import get_ca_bundle_from_env
 
 
+import certifi
 SOCKET_OPTION = typing.Union[
     typing.Tuple[int, int, int],
     typing.Tuple[int, int, typing.Union[bytes, bytearray]],
