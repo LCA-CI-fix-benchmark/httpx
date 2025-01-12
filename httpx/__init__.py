4a57d8e564ff41b68ebade00f082e1d8930dd67d
ubuntu <ubuntu@ip-10-0-1-60>
2025-01-12 15:27:14 +0000

    waleko__claude-3-opus_8820__id_109

--- httpx/__init__.py
+++ httpx/__init__.py
@@ -1,5 +1,3 @@
-from .__version__ import __description__, __title__, __version__
-from ._api import delete, get, head, options, patch, post, put, request, stream
 from ._auth import Auth, BasicAuth, DigestAuth, NetRCAuth
 from ._client import USE_CLIENT_DEFAULT, AsyncClient, Client
 from ._config import Limits, Proxy, Timeout, SSLContext
@@ -44,6 +42,8 @@ from ._transports.wsgi import WSGITransport
 from ._types import AsyncByteStream, SyncByteStream
 from ._urls import URL, QueryParams
 
+from .__version__ import __description__, __title__, __version__
+from ._api import delete, get, head, options, patch, post, put, request, stream
 try:
     from ._main import main
 except ImportError:  # pragma: no cover
