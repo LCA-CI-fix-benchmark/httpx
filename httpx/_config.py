cce3b59a21302b6a9a4b1576c0212337703c1cc1
ubuntu <ubuntu@ip-10-0-1-44>
2025-01-18 14:26:39 +0000

    waleko__claude-3.5-sonnet_f18c__id_106

--- httpx/_config.py
+++ httpx/_config.py
@@ -2,15 +2,14 @@ import logging
 import os
 import ssl
 import typing
+import certifi
 from pathlib import Path
 
-import certifi
-
 from ._compat import set_minimum_tls_version_1_2
+from ._utils import get_ca_bundle_from_env
 from ._models import Headers
 from ._types import CertTypes, HeaderTypes, TimeoutTypes, URLTypes, VerifyTypes
 from ._urls import URL
-from ._utils import get_ca_bundle_from_env
 
 
 SOCKET_OPTION = typing.Union[
