7262685da92f7111593f74521c33a819e4330b16
ubuntu <ubuntu@ip-10-0-1-68>
2025-01-28 17:15:36 +0000

    waleko__llama-3.1-405b_3b20__id_109

--- httpx/__init__.py
+++ httpx/__init__.py
@@ -1,8 +1,7 @@
-from .__version__ import __description__, __title__, __version__
 from ._api import delete, get, head, options, patch, post, put, request, stream
 from ._auth import Auth, BasicAuth, DigestAuth, NetRCAuth
-from ._client import USE_CLIENT_DEFAULT, AsyncClient, Client
-from ._config import Limits, Proxy, Timeout, SSLContext
+from ._client import AsyncClient, Client, USE_CLIENT_DEFAULT
+from ._config import Limits, Proxy, SSLContext, Timeout
 from ._content import ByteStream
 from ._exceptions import (
     CloseError,
@@ -43,6 +42,7 @@ from ._transports.mock import MockTransport
 from ._transports.wsgi import WSGITransport
 from ._types import AsyncByteStream, SyncByteStream
 from ._urls import URL, QueryParams
+from .__version__ import __description__, __title__, __version__
 
 try:
     from ._main import main
