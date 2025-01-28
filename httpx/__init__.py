21d26c3e7533dcbd0029faed2191691d0f1df469
ubuntu <ubuntu@ip-10-0-1-122>
2025-01-28 17:18:49 +0000

    waleko__claude-3.5-sonnet_3001__id_109

--- httpx/__init__.py
+++ httpx/__init__.py
@@ -1,4 +1,3 @@
-from .__version__ import __description__, __title__, __version__
 from ._api import delete, get, head, options, patch, post, put, request, stream
 from ._auth import Auth, BasicAuth, DigestAuth, NetRCAuth
 from ._client import USE_CLIENT_DEFAULT, AsyncClient, Client
@@ -34,6 +33,7 @@ from ._exceptions import (
     WriteError,
     WriteTimeout,
 )
+from .__version__ import __description__, __title__, __version__
 from ._models import Cookies, Headers, Request, Response
 from ._status_codes import codes
 from ._transports.asgi import ASGITransport
