21d6661c842643455a205a9ba79aac384112ea83
ubuntu <ubuntu@ip-10-0-1-6>
2025-01-28 10:43:35 +0000

    waleko__gpt-4o_1d5d__id_109

--- httpx/__init__.py
+++ httpx/__init__.py
@@ -1,9 +1,5 @@
 from .__version__ import __description__, __title__, __version__
 from ._api import delete, get, head, options, patch, post, put, request, stream
-from ._auth import Auth, BasicAuth, DigestAuth, NetRCAuth
-from ._client import USE_CLIENT_DEFAULT, AsyncClient, Client
-from ._config import Limits, Proxy, Timeout, SSLContext
-from ._content import ByteStream
 from ._exceptions import (
     CloseError,
     ConnectError,
@@ -34,6 +30,10 @@ from ._exceptions import (
     WriteError,
     WriteTimeout,
 )
+from ._auth import Auth, BasicAuth, DigestAuth, NetRCAuth
+from ._client import USE_CLIENT_DEFAULT, AsyncClient, Client
+from ._config import Limits, Proxy, Timeout, SSLContext
+from ._content import ByteStream
 from ._models import Cookies, Headers, Request, Response
 from ._status_codes import codes
 from ._transports.asgi import ASGITransport
