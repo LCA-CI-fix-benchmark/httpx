416eae4d3288de3507c9ad9ec55937f6773e43a2
ubuntu <ubuntu@ip-10-0-1-48>
2025-01-29 12:59:39 +0000

    waleko__gemini-pro-1.5_d1da__id_105

--- httpx/_client.py
+++ httpx/_client.py
@@ -995,6 +995,8 @@ class Client(BaseClient):
                 request = self._build_redirect_request(request, response)
                 history = history + [response]
 
+                if self._persistent_cookies:
+                    self.cookies.extract_cookies(response)
                 if follow_redirects:
                     response.read()
                 else:
@@ -1732,6 +1734,8 @@ class AsyncClient(BaseClient):
                 request = self._build_redirect_request(request, response)
                 history = history + [response]
 
+                if self._persistent_cookies:
+                    self.cookies.extract_cookies(response)
                 if follow_redirects:
                     await response.aread()
                 else:
