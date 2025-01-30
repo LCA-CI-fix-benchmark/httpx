8273e87af1fb54d24d607a02041eb01ebcd9e81f
ubuntu <ubuntu@ip-10-0-1-79>
2025-01-30 15:22:35 +0000

    waleko__claude-3-opus_f4f5__id_109

--- httpx/__init__.py
+++ httpx/__init__.py
@@ -1,4 +1,3 @@
-from .__version__ import __description__, __title__, __version__
 from ._api import delete, get, head, options, patch, post, put, request, stream
 from ._auth import Auth, BasicAuth, DigestAuth, NetRCAuth
 from ._client import USE_CLIENT_DEFAULT, AsyncClient, Client
@@ -43,6 +42,7 @@ from ._transports.mock import MockTransport
 from ._transports.wsgi import WSGITransport
 from ._types import AsyncByteStream, SyncByteStream
 from ._urls import URL, QueryParams
+from .__version__ import __description__, __title__, __version__
 
 try:
     from ._main import main
